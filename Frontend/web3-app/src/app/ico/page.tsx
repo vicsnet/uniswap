f93d11c0cce8608eaac2f275763e7668ed25a88e
vicsnet <adesanmiadebisi@gmail.com>
2023-06-01 12:39:53 +0100

    nft mint

---
+++ Frontend/web3-app/src/app/ico/page.tsx
@@ -0,0 +1,7 @@
+
+
+export default function ICO() {
+  return (
+    <div>page</div>
+  )
+}
--- Frontend/web3-app/src/constants/contractAddress.ts
+++ Frontend/web3-app/src/constants/contractAddress.ts
@@ -1,1 +1,3 @@
 export const whitelistAddress = "0x469119640c963340Cca3d3f318AB1EB917Ca22CE";
+
+export const NFT_contractAddress= "0xeE4C5FF6676B89aD6C5B4B458b865230E24d6F80"
